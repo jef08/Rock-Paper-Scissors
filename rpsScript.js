76ffd684f353b72294fe6a25e873cece7d73a24b
jef08 <jasfielding@gmail.com>
2024-07-02 11:57:03 +0200

    All mechanics working

--- rpsScript.js
+++ rpsScript.js
@@ -1,14 +1,33 @@
 //Dom variables//
 const buttons = document.querySelectorAll(".bttn");
-const printHumanScore = document.querySelector("#printHumanScore");
-const printComputerScore = document.querySelector("#printComputerScore");
+const printScores = document.querySelector(".printScores");
 const printHuman = document.querySelector("#printHuman");
 const printComputer = document.querySelector("#printComputer");
 const printWinner = document.querySelector("#printWinner");
+const playAgain = document.querySelector("#playAgain");
 
 //scores//
 let humanScore = 0;
 let computerScore = 0;
+
+//to later disable buttons//
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+//function for playAgain button to refresh page//
+function refreshPage () {
+    location.reload()
+}
+playAgain.addEventListener('click', refreshPage)
+
+//Code for button and text within button// 
+//code for button to appear is with the winner code at the bottom//
+let refreshbutton = document.createElement('button');
+let buttonText = document.createTextNode("Play again?");
+refreshbutton.appendChild(buttonText);
 
 //function through eventlistener on each click//
 buttons.forEach((button) => {
@@ -37,38 +56,43 @@ buttons.forEach((button) => {
         let computerChoice = getComputerChoice();
         let humanChoice = getHumanChoice();
         
+        //Logic for scores and printing//
         if (humanChoice === computerChoice) {
             computerScore++;
             humanScore++;
-            printHumanScore.innerHTML = "You: " + humanScore;
-            printComputerScore.innerHTML = "Computer: " + computerScore;
+            printScores.innerHTML = humanScore + "-" + computerScore ;
             printHuman.innerHTML = "you chose: " + humanChoice;
             printComputer.innerHTML = "The computer chose: " + computerChoice;
+            printWinner.innerHTML = "It's a tie!"
         } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
                     (humanChoice === "paper" && computerChoice === "rock") ||
                     (humanChoice === "scissors" && computerChoice === "paper")) {
                         humanScore++;
-                        printHumanScore.innerHTML = "You: " + humanScore;
-                        printComputerScore.innerHTML = "Computer: " + computerScore;
+                        printScores.innerHTML = humanScore + "-" + computerScore ;
                         printHuman.innerHTML = "you chose: " + humanChoice;
                         printComputer.innerHTML = "The computer chose: " + computerChoice;
+                        printWinner.innerHTML = "You win! :)"
         } else {
             computerScore++;
-            printHumanScore.innerHTML = "You: " + humanScore;
-            printComputerScore.innerHTML = "Computer: " + computerScore;
+            printScores.innerHTML = humanScore + "-" + computerScore ;
             printHuman.innerHTML = "you chose: " + humanChoice;
             printComputer.innerHTML = "The computer chose: " + computerChoice;
+            printWinner.innerHTML = "You lose! :("
         }
         
-        if (humanScore === 5) {
-            printWinner.innerHTML = "You win! :)"
-            button.disabled = true;
-        } else if (computerScore === 5) {
-            printWinner.innerHTML = "You lose! :("
-            button.disabled = true;
+        //print overall winner and disable buttons (window settimeout for later versions)//
+        if (humanScore === 5 && computerScore != 5) {
+            printWinner.innerHTML = "You won the game! :)"
+            disableButtons();
+            playAgain.appendChild(refreshbutton);
+        } else if (computerScore === 5 && humanScore != 5) {
+            printWinner.innerHTML = "You lost the game! :("
+            disableButtons();
+            playAgain.appendChild(refreshbutton);
         } else if (humanScore === 5 && computerScore === 5) {
-            printWinner.innerHTML = "It's a tie!"
-            button.disabled = true;
+            printWinner.innerHTML = "You tied the game!"
+            disableButtons();
+            playAgain.appendChild(refreshbutton);
         }
     }
     })
